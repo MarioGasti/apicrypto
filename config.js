1392addd0f223fc77e87ab4ca8da7cb9bdbe83b2
MarioGasti <gastonferrufino@gmail.com>
2022-01-28 00:47:19 -0300

    make it better

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-process.env.BOT_TOKEN = process.env.BOT_TOKEN || "BOT_TOKEN";
+process.env.BOT_TOKEN =
+    process.env.BOT_TOKEN ||
+    "OTM2MDE4ODk3OTg2MDExMTc4.YfHFhw.JocHDuVKh0GlLojhegi9rb5itJc";
 process.env["X-CMC_PRO_API_KEY"] =
-    process.env["X-CMC_PRO_API_KEY"] || "X-CMC_PRO_API_KEY";
+    process.env["X-CMC_PRO_API_KEY"] || "ea915361-800e-4099-b35a-21b5eadb7ba3";
